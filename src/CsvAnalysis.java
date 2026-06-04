2a39e6aec8f2828650033f288486aef8b17440cf
Emilio Scott <milii76@outlook.com>
2026-06-04 00:12:55 -0700

    patch(csvAnalysis): finish csvAnalysis

--- src/CsvAnalysis.java
+++ src/CsvAnalysis.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,10 +11,6 @@ public class CsvAnalysis implements ICsvAnalysis{
         File dataFile = new File(filename);
         csvData = new ArrayList<>();
         csvCategories = new ArrayList<>();
-
-        // Write the code to handle exceptions when working with Files and Scanners.
-        // Warn the user if an exception is thrown and why. Use a specific exception class, not the Exception class.
-        // Use the Finally block to ensure that the Scanner is closed even when an exception is thrown.
 
         String[] data;
         Scanner scan = null;
@@ -54,7 +49,7 @@ public class CsvAnalysis implements ICsvAnalysis{
 
             csvFileToString = strBuilder.toString();
 
-        } catch(EOFException | FileNotFoundException | FileAlreadyExistsException e){
+        } catch(EOFException | FileNotFoundException e){
             System.err.println("An Error Occured");
             e.printStackTrace();
 
@@ -66,21 +61,16 @@ public class CsvAnalysis implements ICsvAnalysis{
             if (scan != null) scan.close();
 
         }
-        
-        StringBuilder strBuilder = new StringBuilder("");   // For the rest of the lines in the csv file
-        while (scan.hasNextLine()) {                        // first the data are added in the strBuilder to
-            input = scan.nextLine();                        // be converted into a string.
-            strBuilder.append( input );                     // Then they are added in the csvData ArrayList
-            data = input.split(",");                  // so each row of the ArrayList contains the
-            csvData.add( data );                            // data of each dog breed.
-        }
     }
 
     public CsvAnalysis createCsvAnalysis(String filename){ return new CsvAnalysis(filename); }
 
+    @Override
     public String getCsvFileAsString(){ return this.csvFileToString; };
 
+    @Override
     public ArrayList<String> getCsvCategories(){ return this.csvCategories; };
 
+    @Override
     public ArrayList<String[]> getCsvData() { return csvData; }
 }
