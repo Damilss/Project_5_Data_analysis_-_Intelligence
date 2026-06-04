c1521500d73b4199e8f0209a304e0ca93e0f3924
Emilio Scott <milii76@outlook.com>
2026-06-04 00:13:23 -0700

    patch(customException): refine exceptions to adhere to assignment

--- src/EmptyListException.java
+++ src/EmptyListException.java
@@ -1,3 +1,16 @@
-public class EmptyListException extends Exception {
+/**
+ * Custom exception for Assignment! Throwing exception if the list is empty.
+ */
+public class EmptyListException extends RuntimeException {
+    /**
+     * Method overloarding to cover both instances of constructor. One with constuctor with arguments, 
+     * and One constructor without arguments 
+     * */
+    public EmptyListException(){
+        super("EmptyListException, List is empty!");
+    }
     
+    public EmptyListException(String errMessage){
+        super(errMessage);
+    }
 }
--- src/NoResultException.java
+++ src/NoResultException.java
@@ -1,3 +1,16 @@
-public class NoResultException extends Exception {
+/**
+ * Custom exception for Assignment! Throwing exception if the list is empty.
+ */
+public class NoResultException extends RuntimeException {
+    /**
+     * Method overloarding to cover both instances of constructor. One with constuctor with arguments, 
+     * and One constructor without arguments 
+     * */
+    public NoResultException(){
+        super("NoResultException. No result!");
+    }
 
+    public NoResultException(String errMessage){
+        super(errMessage);
+    }
 }
